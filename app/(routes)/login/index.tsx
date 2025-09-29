c1922120798b4e5debf2e90d3fee08810cd611bc
Dehibhis-B <dehibhis@gmail.com>
2025-09-29 14:09:32 -0500

    fonts

--- app/(routes)/login/index.tsx
+++ app/(routes)/login/index.tsx
@@ -1,23 +1,36 @@
 import { View, Text, KeyboardAvoidingView, Platform } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
 import { ScrollView } from 'react-native'
+
 
 export default function LoginScreen() {
   return (
-    <SafeAreaView className='flex-1 bg-white' >
+    <SafeAreaProvider className='flex-1 bg-orange-300'>
+      <SafeAreaView className='flex-1'>
         <KeyboardAvoidingView 
-            behavior={Platform.OS ==="android" ? "padding":"height"}
+          behavior={Platform.OS === "android" ? "padding" : "height"}
+          className='flex-1'
         >
-            <ScrollView 
-                className="flex-1 px-6"
-                showsVerticalScrollIndicator={false}
-                > <View className="mt-16 mb-8" >
-                    <Text className='text-3xl font-poppins-bols text-gray-900 mb-2'>Welcome Back</Text>
-                </View>
-            </ScrollView>        
+          <ScrollView 
+            className="flex-1 px-6"
+            showsVerticalScrollIndicator={false}
+          >
+            <View className="mt-16 mb-8">
+              <Text className='text-3xl font-poppins-bold text-orange-600 mb-2'>
+                Welcome Back
+              </Text>
+            </View>
+            
+            {/* Aquí puedes agregar más contenido */}
+            <View className='bg-white p-6 rounded-lg'>
+              <Text className='text-lg font-inter text-blue-500'>
+                Contenido adicional aquí...
+              </Text>
+            </View>
+          </ScrollView>        
         </KeyboardAvoidingView>
-      
-    </SafeAreaView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   )
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,11 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import { useFonts } from 'expo-font';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
+
+import '../globals.css'
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -11,6 +14,23 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
+
+   const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    "Inter-Regular": require("../assets/fonts/inter-regular.ttf"),
+    "Inter-SemiBold": require("../assets/fonts/inter-semi-bold.ttf"),
+    "Inter-Bold": require("../assets/fonts/inter-bold.ttf"),
+    "Poppins-Regular": require("../assets/fonts/poppins-regular.ttf"),
+    "Poppins-Medium": require("../assets/fonts/poppins-medium.ttf"),
+    "Poppins-Semibold": require("../assets/fonts/poppins-semi-bold.ttf"),
+    "Poppins-Bold": require("../assets/fonts/poppins-bold.ttf"),
+    "Railway": require("../assets/fonts/Railway.ttf"),
+    "Raleway": require("../assets/fonts/raleway.bold.ttf"),
+  })
+
+  if(!loaded){
+    return null
+  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
