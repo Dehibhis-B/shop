d5bb6f18b805084bda18dcc7b3275466252cbdc8
Dehibhis-B <dehibhis@gmail.com>
2025-10-01 17:10:29 -0500

    logins

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,27 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-import { useFonts } from 'expo-font';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
+import { useFonts } from "expo-font";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from "@/hooks/use-color-scheme";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
-import '../globals.css'
+import "../globals.css";
+import Providers from "@/config/provider";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
-   const [loaded] = useFonts({
+  const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
     "Inter-Regular": require("../assets/fonts/inter-regular.ttf"),
     "Inter-SemiBold": require("../assets/fonts/inter-semi-bold.ttf"),
@@ -25,29 +30,33 @@ export default function RootLayout() {
     "Poppins-Medium": require("../assets/fonts/poppins-medium.ttf"),
     "Poppins-Semibold": require("../assets/fonts/poppins-semi-bold.ttf"),
     "Poppins-Bold": require("../assets/fonts/poppins-bold.ttf"),
-    "Railway": require("../assets/fonts/Railway.ttf"),
-    "Raleway": require("../assets/fonts/raleway.bold.ttf"),
-  })
+    Railway: require("../assets/fonts/Railway.ttf"),
+    Raleway: require("../assets/fonts/raleway.bold.ttf"),
+  });
 
-  if(!loaded){
-    return null
+  if (!loaded) {
+    return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <GestureHandlerRootView>
-
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false }} />
-          <Stack.Screen name="(routes)/login/index" options={{headerShown: false}}/>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-        </Stack>
-        <StatusBar style="auto" />
-      
+        <Providers>
+          <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="(routes)/login/index"
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="modal"
+              options={{ presentation: "modal", title: "Modal" }}
+            />
+          </Stack>
+          <StatusBar style="auto" />
+        </Providers>
       </GestureHandlerRootView>
-      
     </ThemeProvider>
   );
 }
